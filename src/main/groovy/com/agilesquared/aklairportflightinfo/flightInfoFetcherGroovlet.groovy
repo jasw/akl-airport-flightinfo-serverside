c4e990e21ebdec06c6d1714bfcde4b25201b2d78
jasonw <jason.lei.wang@gmail.com>
2011-10-29 15:25:40 +1300

    maven build version number clearup

---
+++ src/main/groovy/com/agilesquared/aklairportflightinfo/flightInfoFetcherGroovlet.groovy
@@ -0,0 +1,9 @@
+package com.agilesquared.aklairportflightinfo
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jasonw
+ * Date: 10/03/11
+ * Time: 9:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
